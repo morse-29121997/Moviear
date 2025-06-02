c182fc5460efb3fc3239a9f06612d57ae28fcdf9
mohamed.morse <mohamed.morse@expertapps.com.sa>
2025-06-02 14:14:35 +0300

    Forget to add App ( for apply theme of typograph )

--- composeApp/src/commonMain/kotlin/com/mohamed/morse/moviear/App.kt
+++ composeApp/src/commonMain/kotlin/com/mohamed/morse/moviear/App.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.mohamed.morse.moviear.screens.SplashScreen
+import com.mohamed.morse.moviear.theme.UbuntuTypography
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -19,19 +21,7 @@ import moviear.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+    MaterialTheme(typography = UbuntuTypography()) {
+        SplashScreen()
     }
 }
