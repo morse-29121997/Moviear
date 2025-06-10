7c8ce97215a2a217e5bebcef89a4750ef906e672
mohamed.morse <mohamed.morse@expertapps.com.sa>
2025-06-10 16:17:35 +0300

    Setup some resources for incoming screens

--- composeApp/src/commonMain/kotlin/com/mohamed/morse/moviear/theme/Colors.kt
+++ composeApp/src/commonMain/kotlin/com/mohamed/morse/moviear/theme/Colors.kt
@@ -6,5 +6,9 @@ object Colors {
     object FontColors {
         val white = Color(0XFFFFFFFF)
         val black = Color(0XFF000000)
+        val orange = Color(0XFFFF643A)
+        val gray = Color(0XFF7F8081)
+        val blue = Color(0XFF64D2FF)
+        val lightBlack = Color(0XFF15161B)
     }
 }
