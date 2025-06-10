d780fed28834f8ef05e9801bfcdc748c2b1694e8
mohamed.morse <mohamed.morse@expertapps.com.sa>
2025-06-10 16:20:46 +0300

    Setup profile screen

--- composeApp/src/commonMain/kotlin/com/mohamed/morse/moviear/screens/home/tabs/ProfileContent.kt
+++ composeApp/src/commonMain/kotlin/com/mohamed/morse/moviear/screens/home/tabs/ProfileContent.kt
@@ -1,14 +1,8 @@
 package com.mohamed.morse.moviear.screens.home.tabs
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mohamed.morse.moviear.theme.Colors
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -16,8 +10,8 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
-fun ProfileContent (modifier: Modifier = Modifier){
+fun ProfileContent (){
     Text(
-        "Profile", color = Colors.FontColors.white, fontSize = 18.sp, modifier = modifier
+        "Profile", color = Colors.FontColors.white, fontSize = 18.sp, modifier = Modifier
     )
 }
